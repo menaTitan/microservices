907a80eecca296be8aab3dd5d185bcc382ee8096
Mena <msergeyous@gmail.com>
2020-07-29 13:24:00 -0500

    Project Classes

--- AuthenticationService/src/main/java/com/microservice/App.java
+++ AuthenticationService/src/main/java/com/microservice/App.java
@@ -1,13 +1,13 @@
-package com.webagesolutions.wa2874.AuthenticationService;
+package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationServiceApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServiceApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
